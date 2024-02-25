058ad872f7210a7e7319adddad239cd11e3190d6
oelopes <oelopes2@gmail.com>
2024-02-25 19:06:38 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className={inter.className} lang="pt">
+      <body>{children}</body>
     </html>
   );
 }
